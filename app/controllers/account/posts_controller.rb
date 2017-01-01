e711d2c9bc9f3f9b9f9b6201a17dffb00ba83c2d
lingyou <fl.socialzone@gmail.com>
2017-01-01 21:30:03 +0800

    implement posts pagination with will_paginate

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,6 +1,6 @@
 class Account::PostsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @posts = current_user.posts.recent_update
+    @posts = current_user.posts.recent_update.paginate(:page => params[:page], :per_page => 5)
   end
 end
